5cc1bdcf6485475158c8cf363eddfcb55a43e94d
rajkaushik05 <rajbir.kaushik@srijan.in>
2016-05-18 13:31:12 +0530

    update morning 18-05

--- js/main.js
+++ js/main.js
@@ -1,6 +1,112 @@
+(function($){
+
+	
+	/* on radio button select hide/show expected block */
+	$("#workshopRadioGroup input[type='radio']").change(function(){
+		if($(this).attr('id') == 'checkNotLike' && $(this).is(':checked')) {
+			$('#notLikeComment').slideDown();
+		} else {
+			$('#notLikeComment').slideUp();
+		}
+	});
+
+	$("#counselorRadioGroup input[type='radio']").change(function(){
+		if($(this).attr('id') == 'checkNotLikeCounselor' && $(this).is(':checked')) {
+			$('#notLikeCommentCounselor').slideDown();
+		} else {
+			$('#notLikeCommentCounselor').slideUp();
+		}
+	}); 
 
 
-(function(){
+  	/* Improve page subject add and remove */
+  	$('#changeMindSubjectListedAdd1').on('change', function(){
+  		var selected = this.value;
+  		var optText = selected.split('_').join(' ');
+		$('#changeMindSubjectListed1').append("<li>" + optText + "<span class='btn-delete'></span></li>");
+  	});
+
+	$('#changeMindSubjectListed1').on('click', '.btn-delete', function(){
+		var text = $(this).parent().contents().filter(function() {
+		    return this.nodeType === 3; 
+		});
+		var optVal = text.text().trim();
+		var optValSpace = optVal.split(' ').join('_');
+        if ($('#changeMindSubjectListedAdd1 > option[value="' + optValSpace + '"]').length === 0) {
+           
+		   $('#changeMindSubjectListedAdd1').append("<option value=" + optValSpace + ">" +  optVal + "</option>");
+		} 
+		$(this).parent().remove();
+	});
+
+	$('#changeMindSubjectListedAdd2').on('change', function(){
+  		var selected = this.value;
+  		var optText = selected.split('_').join(' ');
+		$('#changeMindSubjectListed2').append("<li>" + optText + "<span class='btn-delete'></span></li>");
+  	});
+
+	$('#changeMindSubjectListed2').on('click', '.btn-delete', function(){
+		var text = $(this).parent().contents().filter(function() {
+		    return this.nodeType === 3; 
+		});
+		var optVal = text.text().trim();
+		var optValSpace = optVal.split(' ').join('_');
+        if ($('#changeMindSubjectListedAdd2 > option[value="' + optValSpace + '"]').length === 0) {
+           
+		   $('#changeMindSubjectListedAdd2').append("<option value=" + optValSpace + ">" +  optVal + "</option>");
+		} 
+		$(this).parent().remove();
+	});
+
+	/* Register page add editional subject in interest */
+
+	$('#additionalSubjectRegister').on('change', function(){
+  		var selected = this.value;
+  		var optText = selected.split('_').join(' ');
+		$('#defaultSubjectsRegister').append("<li>" + optText + "<span class='btn-delete'></span></li>");
+  	});
+
+	$('#defaultSubjectsRegister').on('click', '.btn-delete', function(){
+		var text = $(this).parent().contents().filter(function() {
+		    return this.nodeType === 3; 
+		});
+		var optVal = text.text().trim();
+		var optValSpace = optVal.split(' ').join('_');
+        if ($('#additionalSubjectRegister > option[value="' + optValSpace + '"]').length === 0) {
+           
+		   $('#additionalSubjectRegister').append("<option value=" + optValSpace + ">" +  optVal + "</option>");
+		} 
+		$(this).parent().remove();
+	});
+
+
+})(jQuery);
+
+
+
+
+/* Register page add editional subject in interest */
+
+	/*document.getElementById('additionalSubjectRegister').addEventListener("change", addSubject);
+	
+	var  listedItem = document.querySelectorAll('#defaultSubjectsRegister li span');
+	for(var i=0; i<listedItem.length; i++){
+		var thisEls = listedItem[i];
+		listedItem.addEventListener('click', deleteItem);
+	};
+
+	function deleteItem(){
+		el = thisEls.parentNode;
+		var elsList = document.getElementById('additionalSubjectRegister');
+		var opt = document.createElement("option");
+		opt.text = el.childNodes[0].nodeValue;
+		elsList.add(opt);
+		console.log();
+		el.style.display = "none";
+
+	}*/
+
+/*(function(){
 	if(document.addEventListener){
 		window.onload = workshopDislike();
 		window.onload = counselorDislike();
@@ -13,6 +119,7 @@
 	};
 	
 	function workshopDislike(){
+		alert();
 		var getEls = document.querySelectorAll("#workshopRadioGroup input[type='radio']");
 		for(var i=0; i<getEls.length; i++){
 			var thisEls = getEls[i];
@@ -40,38 +147,7 @@
 		};
 	};
 
-	/* Register page add editional subject in interest */
-
-	document.getElementById('additionalSubjectRegister').addEventListener("change", addSubject);
-
-	function addSubject(){
-		var selected = this.options[this.selectedIndex].value;
-		var items  = document.getElementById('defaultSubjectsRegister');
-		
-		items.innerHTML += "<li>" + selected + "<button class='btn-delete'></button></li>";
-		console.log(selected);
-	};
-
-
-	var  listedItem = document.querySelectorAll('#defaultSubjectsRegister li button');
-	for(var i=0; i<listedItem.length; i++){
-		var thisEls = listedItem[i];
-		thisEls.addEventListener('click', deleteItem);
-	};
-
-	function deleteItem(){
-		alert("hello");
-		return false
-	}
-
-
-	
-	
-
-
-
-
-})();
+})();*/
 
 
 
