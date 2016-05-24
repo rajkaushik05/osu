2627373029b6756954b02bedb278279162b10648
rajkaushik05 <rajbir.kaushik@srijan.in>
2016-05-24 11:04:26 +0530

    update 23-05

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 (function($){
-
-	
 	/* on radio button select hide/show expected block */
 	$("#workshopRadioGroup input[type='radio']").change(function(){
 		if($(this).attr('id') == 'checkNotLike' && $(this).is(':checked')) {
@@ -17,7 +15,6 @@
 			$('#notLikeCommentCounselor').slideUp();
 		}
 	}); 
-
 
   	/* Improve page subject add and remove */
   	$('#changeMindSubjectListedAdd1').on('change', function(){
@@ -60,6 +57,14 @@
 
 	/* Register page add editional subject in interest */
 
+	$('#considerSubjectRegister').on('change', function(){
+		if(this.value == 'commerce_with_maths'){
+			$('#defaultSubjectsRegister, #additionalSubjectRegister').show();
+		} else {
+			$('#defaultSubjectsRegister, #additionalSubjectRegister').hide();
+		}
+	});
+
 	$('#additionalSubjectRegister').on('change', function(){
   		var selected = this.value;
   		var optText = selected.split('_').join(' ');
@@ -79,75 +84,307 @@
 		$(this).parent().remove();
 	});
 
+	/* Code for Star Rating */
+	$(".rating-block input[type='radio']").on('change', function(){
+		$(this).prevAll("input[type='radio']").addClass('active');
+		$(this).nextAll("input[type='radio']").removeClass('active');
+	});
+
+
+	/*Register Form page-1 form validation */
+	$('.register-first-form-fields input, .register-first-form-fields select').on('blur change', function(){
+		registerFirstFormFields();
+	});
+
+	function registerFirstFormFields(){
+		var yourName = $('.register-first-form-fields #yourName');
+		var classSection = $('.register-first-form-fields #classSection');
+		var schoolText = $('.register-first-form-fields #schoolText');
+		var considerSubjectRegister = $('.register-first-form-fields #considerSubjectRegister');
+		var interestedIn = $('.register-first-form-fields #interestedIn');
+		var registerFirstForm = [yourName, classSection, schoolText, considerSubjectRegister, interestedIn];
+		var count = 1;
+		var ratio = 7.2;
+		var finalStatus = 0;
+	 	for(var i=0; i<registerFirstForm.length; i++){
+	 		var str = registerFirstForm[i].val();
+	 		if(typeof str === 'string' && str != '') {
+	 			var getStatus = ratio * count;
+	 			var finalStatus = getStatus;
+	 			count++;
+	 		}
+	 	}
+	 	$('.my-bar').css('width', finalStatus + "%");
+	 	finalStatus = Math.round(finalStatus);
+	 	$('.my-bar').text(finalStatus + "%");
+	};
+
+	$('#registerFirstFormFieldsNext').click(function(){
+		var yourName = $('.register-first-form-fields #yourName');
+		var classSection = $('.register-first-form-fields #classSection');
+		var schoolText = $('.register-first-form-fields #schoolText');
+		var considerSubjectRegister = $('.register-first-form-fields #considerSubjectRegister');
+		var interestedIn = $('.register-first-form-fields #interestedIn');
+		var registerFirstForm = [yourName, classSection, schoolText, considerSubjectRegister, interestedIn];
+		var count = 1;
+	 	for(var i=0; i<registerFirstForm.length; i++){
+	 		var str = registerFirstForm[i].val();
+	 		if(typeof str === 'string' && str != '') {
+	 			count++;
+	 		}
+	 	}
+	 	if(count-1 == registerFirstForm.length){
+	 		$(this).attr('href', 'register-1.html');
+	 	}
+		
+	});
+	
+	/*Register Form page-2 form validation */
+	$('.register-second-form-fields input, .register-second-form-fields select').on('blur change', function(){
+		registerSecondFormFields();
+	});
+
+	function registerSecondFormFields(){
+		var candidateNumber = $('.register-second-form-fields #candidateNumber');
+		var candidateEmail = $('.register-second-form-fields #candidateEmail');
+		var momPhone = $('.register-second-form-fields #momPhone');
+		var momEmail = $('.register-second-form-fields #momEmail');
+		var dadPhone = $('.register-second-form-fields #dadPhone');
+		var dadEmail = $('.register-second-form-fields #dadEmail');
+		var guardianPhone = $('.register-second-form-fields #guardianPhone');
+		var guardianEmail = $('.register-second-form-fields #guardianEmail');
+		var registerSecondForm = [candidateNumber, candidateEmail, momPhone, momEmail, 
+		dadPhone, dadEmail, guardianPhone, guardianEmail];
+		var count = 1;
+		var ratio = 7.2;
+		var finalStatus = 0;
+	 	for(var i=0; i<registerSecondForm.length; i++){
+	 		var str = registerSecondForm[i].val();
+	 		if(typeof str === 'string' && str != '') {
+	 			var getStatus = ratio * count;
+	 			var finalStatus = finalStatus + getStatus;
+	 			count++;
+	 		}
+	 	}
+	 	finalStatus += 36;
+	 	$('.my-bar').css('width', finalStatus + "%");
+	 	finalStatus  = Math.round(finalStatus);
+	 	$('.my-bar').text(finalStatus + "%");
+	};
+
+	$('#registerSecondFormFieldsNext').click(function(){
+		var candidateNumber = $('.register-second-form-fields #candidateNumber');
+		var candidateEmail = $('.register-second-form-fields #candidateEmail');
+		var momPhone = $('.register-second-form-fields #momPhone');
+		var momEmail = $('.register-second-form-fields #momEmail');
+		var dadPhone = $('.register-second-form-fields #dadPhone');
+		var dadEmail = $('.register-second-form-fields #dadEmail');
+		var guardianPhone = $('.register-second-form-fields #guardianPhone');
+		var guardianEmail = $('.register-second-form-fields #guardianEmail');
+		var registerSecondForm = [candidateNumber, candidateEmail, momPhone, momEmail, 
+		dadPhone, dadEmail, guardianPhone, guardianEmail];
+		var count = 1;
+	 	for(var i=0; i<registerSecondForm.length; i++){
+	 		var str = registerSecondForm[i].val();
+	 		if(typeof str === 'string' && str != '') {
+	 			count++;
+	 		}
+	 	}
+		if(count-1 == registerSecondForm.length){
+			$(this).attr('href', 'register-2.html');
+		}
+	});
+
+
+	/*Register Form page-3 form validation */
+
+	$(".register-third-form-fields input[type='radio']").change(function(){
+		registerThirdFormFields();
+	});
+
+	function registerThirdFormFields(){
+		var videoLikeDislikRadio = $("#videoLikeDislikRadio input[type='radio']");
+		var count = 1;
+		var ratio = 6.4;
+		var finalStatus = 0;
+		if(videoLikeDislikRadio.is(':checked')){
+			$('#videoLikeDislikRadio .alert-error').slideUp();
+			var getStatus = ratio * count;
+	 		var finalStatus = finalStatus + getStatus;
+		}
+		finalStatus += 93.6;
+	 	$('.my-bar').css('width', finalStatus + "%");
+	 	finalStatus  = Math.round(finalStatus);
+	 	$('.my-bar').text(finalStatus + "%");
+	};
+	
+	$('#videoLikeDislikRadioValidation').click(function(){
+		if($("#videoLikeDislikRadio input[type='radio']").is(':checked')) {
+			$(this).attr('href', 'improve.html');
+			$('#videoLikeDislikRadio .alert-error').slideUp();
+		} else {
+			$('#videoLikeDislikRadio .alert-error').slideDown();
+		}
+	});
+
+
+ 	/*==============Improve Form page-1 form validation ==========================*/
+
+	$('.improve-first-form-fields input, .improve-first-form-fields select').on('blur change', function(){
+		improveFirstFormFields();
+	});
+
+	function improveFirstFormFields(){
+		var updateYourName = $('.improve-first-form-fields #updateYourName');
+		var updateClassAndSection = $('.improve-first-form-fields #updateClassAndSection');
+		var updateSchoolName = $('.improve-first-form-fields #updateSchoolName');
+
+		var allfields = [updateYourName, updateClassAndSection, updateSchoolName];
+		var count = 1;
+		var ratio = 11.11;
+		var finalStatus = 0;
+	 	for(var i=0; i<allfields.length; i++){
+	 		var str = allfields[i].val();
+	 		if(typeof str === 'string' && str != '') {
+	 			var getStatus =  ratio * count;
+	 			var finalStatus = getStatus;
+	 			count++;
+	 		}
+	 	}
+	 	$('.my-bar').css('width', finalStatus + "%");
+	 	finalStatus = Math.round(finalStatus);
+	 	$('.my-bar').text(finalStatus + "%");
+	};
+
+	$('#improveFirstFormFieldsNext').click(function(){
+		var updateYourName = $('.improve-first-form-fields #updateYourName');
+		var updateClassAndSection = $('.improve-first-form-fields #updateClassAndSection');
+		var updateSchoolName = $('.improve-first-form-fields #updateSchoolName')
+		var allfields = [updateYourName, updateClassAndSection, updateSchoolName];
+		var count = 1;
+	 	for(var i=0; i<allfields.length; i++){
+	 		var str = allfields[i].val();
+	 		if(typeof str === 'string' && str != '') {
+	 			count++;
+	 		}
+	 	}
+	 	if(count-1 == allfields.length){
+	 		$(this).attr('href', 'improve-1.html');
+	 	}
+	});
+
+
+	/*Improve Form page-2 form validation */
+	$('.improve-second-form-fields input, .improve-second-form-fields textarea').on('blur change', function(){
+		improveSecondFormFields();
+	});
+
+	function improveSecondFormFields(){
+		var workshopStarRating = $(".improve-second-form-fields #workshopStarRating input[type='radio']");
+		var workshopRadioGroup = $(".improve-second-form-fields #workshopRadioGroup input[type='radio']");
+		var workShopOtherComment = $('.improve-second-form-fields #workShopOtherComment');
+		var allfields = [workshopStarRating, workshopRadioGroup];
+		var count = 1;
+		var ratio = 11.11;
+		var finalStatus = 0;
+	 	for(var i=0; i<allfields.length; i++){
+	 		var el = allfields[i];
+	 		if($(el).is(':checked')){
+	 			var getStatus = ratio * count;
+	 			var finalStatus = getStatus;
+	 			count++;
+	 		}
+	 	};
+	 	var str = workShopOtherComment.val();
+	 	if(str != '') {
+	 		finalStatus += 11.11;
+	 	};
+
+	 	finalStatus += 33;
+	 	$('.my-bar').css('width', finalStatus + "%");
+	 	finalStatus = Math.round(finalStatus);
+	 	$('.my-bar').text(finalStatus + "%");
+	};
+
+	$('#improveSecondFormFieldsNext').click(function(){
+		var workshopStarRating = $(".improve-second-form-fields #workshopStarRating input[type='radio']");
+		var workshopRadioGroup = $(".improve-second-form-fields #workshopRadioGroup input[type='radio']");
+		var workShopOtherComment = $('.improve-second-form-fields #workShopOtherComment');
+		var allfields = [workshopStarRating, workshopRadioGroup];
+		var count = 1;
+		var ratio = 11.11;
+		var finalStatus = 0;
+	 	for(var i=0; i<allfields.length; i++){
+	 		var el = allfields[i];
+	 		if($(el).is(':checked')){
+	 			var getStatus = ratio * count;
+	 			var finalStatus = getStatus;
+	 			count++;
+	 		}
+	 	};
+	 	var str = workShopOtherComment.val();
+	 	if(str != '') {
+	 		finalStatus += 11.11;
+	 	};
+
+	 	finalStatus += 33;
+	 	if(finalStatus == 66.33){
+	 		$(this).attr('href', 'improve-2.html');
+	 	}
+	});
+
+
+	/*Improve Form page-3 form validation */
+	$('.improve-third-form-fields input, .improve-third-form-fields textarea').on('blur change', function(){
+		improveThirdFormFields();
+	});
+
+	function improveThirdFormFields(){
+		var counselorStarRating = $(".improve-third-form-fields #counselorStarRating input[type='radio']");
+		var counselorRadioGroup = $(".improve-third-form-fields #counselorRadioGroup input[type='radio']");
+		var counselorOtherComment = $('.improve-third-form-fields #counselorOtherComment');
+		var allfields = [counselorStarRating, counselorRadioGroup];
+		var count = 1;
+		var ratio = 11.11;
+		var finalStatus = 0;
+	 	for(var i=0; i<allfields.length; i++){
+	 		var el = allfields[i];
+	 		if($(el).is(':checked')){
+	 			var getStatus = ratio * count;
+	 			var finalStatus = getStatus;
+	 			count++;
+	 		}
+	 	};
+	 	var str = counselorOtherComment.val();
+	 	if(str != '') {
+	 		finalStatus += 11.11;
+	 	};
+
+	 	finalStatus += 67;
+	 	$('.my-bar').css('width', finalStatus + "%");
+	 	finalStatus = Math.round(finalStatus);
+	 	$('.my-bar').text(finalStatus + "%");
+	};
+
+	$('#improveThirdFormFieldsNext').click(function(){
+		return false
+	});
+
+	/* Input fields editable and Undo */
+	$('.editable .field-edit').click(function(){
+		$(this).hide();
+		$(this).siblings('.field-undo').show();
+		$(this).siblings('input').prop('disabled', false);
+		$(this).siblings('input').focus();
+	});
+
+	$('input').focus(function(){
+		this.value = this.value;
+	});
+
+
 
 })(jQuery);
-
-
-
-
-/* Register page add editional subject in interest */
-
-	/*document.getElementById('additionalSubjectRegister').addEventListener("change", addSubject);
-	
-	var  listedItem = document.querySelectorAll('#defaultSubjectsRegister li span');
-	for(var i=0; i<listedItem.length; i++){
-		var thisEls = listedItem[i];
-		listedItem.addEventListener('click', deleteItem);
-	};
-
-	function deleteItem(){
-		el = thisEls.parentNode;
-		var elsList = document.getElementById('additionalSubjectRegister');
-		var opt = document.createElement("option");
-		opt.text = el.childNodes[0].nodeValue;
-		elsList.add(opt);
-		console.log();
-		el.style.display = "none";
-
-	}*/
-
-/*(function(){
-	if(document.addEventListener){
-		window.onload = workshopDislike();
-		window.onload = counselorDislike();
-		//window.onload = myFunction();
-
-
-	} else if(document.attachEvent) {
-		window.onload = workshopDislike();
-		window.onload = counselorDislike();
-	};
-	
-	function workshopDislike(){
-		alert();
-		var getEls = document.querySelectorAll("#workshopRadioGroup input[type='radio']");
-		for(var i=0; i<getEls.length; i++){
-			var thisEls = getEls[i];
-			thisEls.addEventListener("change", function(){
-				if(thisEls.id == "checkNotLike" && thisEls.checked == true){
-					document.getElementById("notLikeComment").style.display = "block";
-				} else {
-					document.getElementById("notLikeComment").style.display = "none";
-				};
-			});
-		};
-	};
-
-	function counselorDislike(){
-		var getEls = document.querySelectorAll("#counselorRadioGroup input[type='radio']");
-		for(var i=0; i<getEls.length; i++){
-			var thisEls = getEls[i];
-			thisEls.addEventListener("change", function(){
-				if(thisEls.id == "checkNotLikeCounselor" && thisEls.checked == true){
-					document.getElementById("notLikeCommentCounselor").style.display = "block";
-				} else {
-					document.getElementById("notLikeCommentCounselor").style.display = "none";
-				};
-			});
-		};
-	};
-
-})();*/
 
 
 
